249d084d35fb22dec7952422e9e5687790f18228
Leah <102230369+LoopyLeah@users.noreply.github.com>
2024-07-24 14:10:41 +0100

    Update myfan.sh

--- myfan.sh
+++ myfan.sh
@@ -9,22 +9,31 @@ THRESHOLD_DISENGAGED=85
 # File to read temperature from
 TEMP_FILE="/sys/class/hwmon/hwmon4/temp1_input"
 
-# Read current temperature
-CURRENT_TEMP=$(cat $TEMP_FILE)
-CURRENT_TEMP_C=$((CURRENT_TEMP / 1000))
+# Delay between checks (in seconds)
+DELAY=10
 
-# Determine fan level
-if [ "$CURRENT_TEMP_C" -ge "$THRESHOLD_DISENGAGED" ]; then
-  FAN_LEVEL="disengaged"
-elif [ "$CURRENT_TEMP_C" -ge "$THRESHOLD_3" ]; then
-  FAN_LEVEL=7
-elif [ "$CURRENT_TEMP_C" -ge "$THRESHOLD_2" ]; then
-  FAN_LEVEL=4
-elif [ "$CURRENT_TEMP_C" -ge "$THRESHOLD_1" ]; then
-  FAN_LEVEL=2
-else
-  FAN_LEVEL=1
-fi
+while true; do
+  # Read current temperature
+  CURRENT_TEMP=$(cat $TEMP_FILE)
+  CURRENT_TEMP_C=$((CURRENT_TEMP / 1000))
 
-# Set fan speed
-echo level $FAN_LEVEL > /proc/acpi/ibm/fan
+  # Determine fan level
+  if [ "$CURRENT_TEMP_C" -ge "$THRESHOLD_DISENGAGED" ]; then
+    FAN_LEVEL="disengaged"
+  elif [ "$CURRENT_TEMP_C" -ge "$THRESHOLD_3" ]; then
+    FAN_LEVEL=7
+  elif [ "$CURRENT_TEMP_C" -ge "$THRESHOLD_2" ]; then
+    FAN_LEVEL=4
+  elif [ "$CURRENT_TEMP_C" -ge "$THRESHOLD_1" ]; then
+    FAN_LEVEL=2
+  else
+    FAN_LEVEL=1
+  fi
+
+  # Set fan speed
+  echo "Current temperature: $CURRENT_TEMP_C°C. Setting fan to $FAN_LEVEL."
+  echo level $FAN_LEVEL > /proc/acpi/ibm/fan
+
+  # Wait before the next check
+  sleep $DELAY
+done
